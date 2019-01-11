b0de0e1c6d239c283b74ff55c8d9799fb5526907
leonmiriti <leonmiriti@10.44.28.55>
2019-01-11 13:41:02 +0100

    new binary search commit 2019

--- BinarySearch.js
+++ BinarySearch.js
@@ -22,5 +22,5 @@ var doSearch = function(array, targetValue) {
 var primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 
 		41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97];
 
-var result = doSearch(primes, 11);
+var result = doSearch(primes, 67);
 console.log("Found prime at index " + result);
